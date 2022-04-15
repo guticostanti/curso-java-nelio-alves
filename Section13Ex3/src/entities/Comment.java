76c9c632f7716baac1c827ccfa880e10b5db7d3f
guticostanti <gustavo.costanti.lara@gmail.com>
2022-04-15 10:09:03 -0300

    21 - Section 13 - Ex 3

---
+++ Section13Ex3/src/entities/Comment.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Comment {
+
+	private String text;
+
+	public Comment(String test) {
+		this.text = test;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setTest(String test) {
+		this.text = test;
+	}
+	
+	
+}
