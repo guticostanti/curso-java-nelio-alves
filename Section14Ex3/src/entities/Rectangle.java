ac75874b2a6d773d6711c641cb7f6976b8243ca8
guticostanti <gustavo.costanti.lara@gmail.com>
2022-04-19 05:17:56 -0300

    27 - Section 14 - Ex 3

---
+++ Section14Ex3/src/entities/Rectangle.java
@@ -0,0 +1,21 @@
+package entities;
+
+import entities.enums.Color;
+
+public class Rectangle extends Shape {
+
+	private Double width;
+	private Double height;
+	
+	public Rectangle() {
+		super();
+	}
+	
+	public Rectangle(Color color, Double width, Double height) {
+		super(color);
+		this.width = width;
+		this.height = height;
+	}
+	
+	
+}
