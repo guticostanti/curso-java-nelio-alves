c67ab0f67c23b0a8e9fb268a7ab532f65f5f1816
guticostanti <gustavo.costanti.lara@gmail.com>
2022-04-20 08:07:39 -0300

    30 - Section14 - Ex 4 Part 2

--- Section14Ex4/src/application/Program.java
+++ Section14Ex4/src/application/Program.java
@@ -1,9 +1,50 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Company;
+import entities.Individual;
+import entities.TaxPayer;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<TaxPayer> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of tay payers: ");
+		int numberOfTaxPayers = sc.nextInt();
+		for (int i = 1; i<=numberOfTaxPayers; i++) {
+			System.out.println("Tax payer #" + i + " data:");
+			System.out.print("Individual or company (i/c)? ");
+			char type = sc.next().charAt(0);
+			System.out.print("Name: ");
+			String name = sc.next();
+			System.out.print("Anual income: ");
+			Double anualRevenue = sc.nextDouble();
+			if (type == 'i') {
+				System.out.print("Health expenditures: ");
+				Double healthSpending = sc.nextDouble();
+				list.add(new Individual(name, anualRevenue, healthSpending));
+			} else if (type == 'c') {
+				System.out.print("Number of employees: ");
+				int numberOfEmployees = sc.nextInt();
+				list.add(new Company(name, anualRevenue, numberOfEmployees));
+			} else {
+				System.out.print("You entered and invalid type of Tax Payer");
+			}
+			
+		sc.close();
+		System.out.println();
+		System.out.println("TAXES PAID:");
+		for (TaxPayer taxPayer : list) {
+			
+		}
 
 	}
 
